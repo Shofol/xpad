10c5279f3f6d0dad9157b2e6a2e95fbc92996c6a
shofol <jahananower@gmail.com>
2021-06-11 16:58:24 +0600

    add blur on nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,12 +7,15 @@ import {
 
 const Navbar = () => {
     return (
-        <nav className="max-w-sm md:max-w-xl md:mx-auto lg:max-w-full mx-6 lg:mx-28 flex justify-between items-start font-bold">
-            <Link to="/" className={`${styles.logoBorder} px-8 py-1 bg-xpad-grad text-sm lg:px-12 lg:py-2 lg:text-3xl`}>Xpad</Link>
-            <div className="text-gray-100 flex justify-between items-center self-end text-xs lg:text-2xl">
-                <a href="/#aboutus" className=" hover:bg-xpad-grad hover:text-black rounded-md px-3 lg:px-5 py-1">About</a>
-                <a href="/#tokenomics" className=" hover:bg-xpad-grad hover:text-black rounded-md px-3 lg:px-5 py-1">Tokenomics</a>
-                <a href="/#connect" className=" hover:bg-xpad-grad hover:text-black rounded-md px-3 lg:px-5 py-1">Connect</a>
+        <nav className={`z-10 fixed left-0 right-0 top-0 w-full pb-10`}>
+            <div className={`${styles.navBg}`} style={{ backgroundImage: `${process.env.PUBLIC_URL}/background.svg` }}></div>
+            <div className="max-w-sm md:max-w-xl md:mx-auto lg:max-w-full mx-6 lg:mx-28 flex justify-between items-start font-bold relative">
+                <Link to="/" className={`${styles.logoBorder} px-8 py-1 bg-xpad-grad text-sm lg:px-12 lg:py-2 lg:text-3xl`}>Xpad</Link>
+                <div className="text-gray-100 flex justify-between items-center self-end text-xs lg:text-2xl">
+                    {/* <a href="/#about" className=" hover:bg-xpad-grad hover:text-black rounded-md px-3 lg:px-5 py-1">About</a>
+                <a href="/#tokenomics" className=" hover:bg-xpad-grad hover:text-black rounded-md px-3 lg:px-5 py-1">Tokenomics</a> */}
+                    <a href="/#contact" className=" hover:bg-xpad-grad hover:text-black rounded-md px-3 lg:px-5 py-1">Connect</a>
+                </div>
             </div>
         </nav>
     )
